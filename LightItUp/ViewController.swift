51ef15f71c40114fdf8295b79faefe59501a7d28
Bruce <bruce.duffy@fiu.edu>
2022-10-24 15:55:15 -0400

    Monday, Class Day 1. Moving on to the final exercise.

--- LightItUp/ViewController.swift
+++ LightItUp/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - IB Outlets
+    
+    @IBOutlet weak var lightButton: UIButton!
+    
+        
+    // MARK: - Instance Properties
+    
+    var lightOn = false
+    
+    
+    // MARK: - VDL
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        upateUI()
     }
 
+    // MARK: - IB Actions
+    
+    @IBAction func powerButtonTapped(_ sender: UIButton) {
+        upateUI()
+    }
+    
+    // MARK: - Instance Methods
+    
+    fileprivate func upateUI() {
+        // lightOn = !lightOn
+        lightOn.toggle()
+        if lightOn {
+            view.backgroundColor = .white
+            lightButton.setTitle("Off", for: .normal)
+        } else {
+            view.backgroundColor = .black
+            lightButton.setTitle("On", for: .normal)
+            lightButton.setTitleColor(.white, for: .normal)
+        }
+    }
 
 }
 
